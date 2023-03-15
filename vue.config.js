415919fa0461cffd0f126773869d505725771fdd
auwerk <noisefront@yandex.ru>
2023-03-15 17:10:24 +0300

    Basic setup

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api/user/register': {
+        target: 'http://arch.homework',
+        changeOrigin: true,
+      },
+    },
+  },
 })
